e056f3eed1f1c7524bf3f77c75701af52576ec05
malikolabiyi <olabiyimalik2014@gmail.com>
2019-11-22 02:53:57 +0100

    Creating

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def analyse():
         focus_keyword = request.form['focus_keyword']
         content = request.args.GET['content']
 
-    ''' Headline '''
+    ''' HEADLINE '''
+    headline_score = 0
 
     ''' Word Balance '''
 
@@ -35,9 +36,10 @@ def analyse():
         number_power_words = len(power_words)
 
         if number_power_words > 0:
-            power_word_analysis = "Your content have " + str(number_power_words) + " power words, they are " + str(power_words)  + ". Power words also known as phrases indicates intense trigger words that frequently command a reader's attention and action."
+            power_word_analysis = "Your headline have " + str(number_power_words) + " power words. Power words also known as phrases indicates intense trigger words that frequently command a reader's attention and action."
+            headline_score += 30
         else:
-            power_word_analysis: 'Your content ought to contain atleast one power word.'
+            power_word_analysis: 'Your headline ought to contain atleast one power word.'
     p.close()
 
     #Common words
@@ -50,9 +52,10 @@ def analyse():
         number_common_words = len(common_words)
 
         if number_common_words > 0:
-            common_word_analysis = "Good! Your content have " + str(number_common_words) + " common words. Common words make up the basic structure of readable headlines. It is recommended that a headline should be made up of 2 to 3 common words."
+            common_word_analysis = "Good! Your headline have " + str(number_common_words) + " common words. Common words make up the basic structure of readable headlines. It is recommended that a headline should be made up of 2 to 3 common words."
+            headline_score += 10
         else:
-            common_word_analysis: 'Your content ought to contain atleast 2 common words. Common words make up the basic structure of readable headlines.'
+            common_word_analysis: 'Your headline ought to contain atleast 2 common words. Common words make up the basic structure of readable headlines.'
     c.close()
 
     #Uncommon words
@@ -65,28 +68,45 @@ def analyse():
         number_uncommon_words = len(uncommon_words)
 
         if number_uncommon_words > 0:
-            uncommon_word_analysis = "Good! Your content have " + str(number_uncommon_words) + " uncommon words. Uncommon words should occur less frequently than common words, but it is recommended to give your headline substance by adding 2 uncommon words."
+            uncommon_word_analysis = "Good! Your headline have " + str(number_uncommon_words) + " uncommon words. Uncommon words should occur less frequently than common words, but it is recommended to give your headline substance by adding 2 uncommon words."
+            headline_score += 10
         else:
-            uncommon_word_analysis = 'Your content ought to contain atleast a uncommon word. Uncommon words should occur less frequently than common words, but it is recommended to give your headline substance by adding 2 uncommon words.'
+            uncommon_word_analysis = 'Your headline ought to contain atleast a uncommon word. Uncommon words should occur less frequently than common words, but it is recommended to give your headline substance by adding 2 uncommon words.'
     uc.close()
 
-    ''' Word Length for headline '''
+    ''' SEO for headline '''
+    # For focus keyword in headline
+    if focus_keyword.lower() in headline.lower():
+        keyword_in_headline = 'Great! Your focus keyword appeared in your headline.'
+        headline_score += 20
+    else:
+        keyword_in_headline = "Oops! Your focus keyword didn't appear in your headline."
+
+    # Number in headline
+    num = int()
+    for word in headline.split(' '):
+        if type(word) == int or type(word) == float:
+            if word % 2 != 0:
+                number_in_headline = 'Amazing! Some numbers appeared in your headline. This helps to improve your click-through-rate on the search engine.'
+                headline_score += 10
+            else:
+                number_in_headline = 'Though some figures appeared but it is an even number, it is better if an odd number appeared.'
+        else:
+            number_in_headline = 'Your headline doesn\'t contain a figure, it is recommended to have a figure especially if you\'re writing on listing topics like top 9 places to visit. This helps to improve your click-through-rate on the search engine.'
+
+    # Word Length for headline
 
     if len(re.findall(r'\w+', headline)) < 3:
         word_length_for_headline = 'Your headline word length is kind of small, can you try to add more words?'   
     elif len(re.findall(r'\w+', headline))  > 20:
         word_length_for_headline = 'Your headline word length is too much, can you try to reduce them?'
     else: 
-        word_length_for_headline = 'Awesome! Your headline word length is just perfect!' 
-
-    ''' SEO for headline '''
-    # For focus keyword in headline
-    if focus_keyword.lower() in headline.lower():
-        keyword_in_headline = 'Great! Your focus keyword appeared in your headline.'
-    else:
-        keyword_in_headline = "Oops! Your focus keyword didn't appear in your headline."
-
+        word_length_for_headline = 'Awesome! Your headline word length is just perfect!'
+        headline_score += 20 
+    
+    
     ''' Content '''
+    content_score = 0
 
     soup = BeautifulSoup(content, 'lxml')
     content_without_tags = soup.text
@@ -139,46 +159,90 @@ def analyse():
     # Keyword in first paragraph
     paragraphs = []
 
-    paragraphs = soup.find_all('p')
+    for paragraph in soup.find_all('p'):
+        paragraphs.append(paragraph)
 
     if focus_keyword in paragraphs[0]:
         keyword_in_first_paragraph = 'Your focus keyword appeared in the first paragraph of your content, +1 for this!'
     else:
         keyword_in_first_paragraph = "Your focus keyword ought to appear in your first paragraph, incase you don't have a meta description, this is going to show on the search engine."
-
+    
     # Paragraph Length.
     good_paragraph_length = []
     bad_paragraph_length = []
 
-    words_number = []
-
     for paragraph in paragraphs:
         paragraph = paragraph.text
-        for words in paragraph.split(' '):
-            words_number.append(words)
 
-            if len(words_number) < 50:
-                good_paragraph_length.append(paragraph)
-            else:
-                bad_paragraph_length.append(paragraph)
+        if len(paragraph.split(' ')) < 50:
+            good_paragraph_length.append(paragraph)
+        else:
+            bad_paragraph_length.append(paragraph)
 
-    paragraph_analysis = 'Your content has ' + str(len(paragraphs)) + " paragraphs, only " + str(len(good_paragraph_length)) + "paragraphs contains the recommended number of words while " + str(len(bad_paragraph_length)) + " paragraphs contain much words than recommended."
+    paragraph_analysis = 'Your content has ' + str(len(paragraphs)) + " paragraphs, only " + str(len(good_paragraph_length)) + " paragraphs contains the recommended number of words while " + str(len(bad_paragraph_length)) + " paragraphs contain much words than recommended."
 
     # Heading
 
     heading_tags = ['h2', 'h3', 'h4', 'h5', 'h6']
 
-    headings = []
-
-    for heading_tag in heading_tags:
-        headings = soup.find_all(heading_tag)
+    headings = soup.find_all(heading_tags)
     
     if len(headings) > 0:
         heading_analysis = 'Your content contains some headings, this gives your content more taste!'
     else: 
         heading_analysis = 'Your content is missing some headings.'
+
+    heading_with_focus_keyword = []
+
+    heading_without_focus_keyword = []
+
+    # Focus keyword in heading
+
+    for heading in headings:
+        heading = heading.text
+        if focus_keyword in heading:
+            heading_with_focus_keyword.append(heading)
+        else:
+            heading_without_focus_keyword.append(heading)
     
-    return render_template('analyse.html', heading_analysis=heading_analysis, keyword_in_first_paragraph=keyword_in_first_paragraph, paragraph_analysis=paragraph_analysis, uncommon_word_analysis=uncommon_word_analysis, common_word_analysis=common_word_analysis, power_word_analysis=power_word_analysis, keyword_in_headline=keyword_in_headline, word_length_for_headline=word_length_for_headline, reading_ease_score=reading_ease_score, grade_level_score=grade_level_score, content_word_length=content_word_length)
+    heading_focus_keyword = "Your content has " + str(len(headings)) + " heading(s) only " + str(len(heading_with_focus_keyword)) + " heading(s) contains your focus keyword while " + str(len(heading_without_focus_keyword)) + " heading(s) doesn't contain your focus keyword. It is recommended that your focus keyword should appear atleast in 2 heading but not all to avoid keyword stuffing." 
+    
+    # Image
+    images = []
+
+    for image in soup.find_all('img'):
+        images.append(image)
+    
+    if len(images) > 0:
+        check_image = 'Nice! Image(s) appeared in your content.'
+    else:
+        check_image = 'An image are missing in your content. Images gives the visual expression of what your content is talking about, atleast one image should appeared in your content.'
+
+    # Link
+    links = soup.find_all('a')
+
+    if len(links) > 0:
+        links_analysis = 'It is so much cool that you content is linking to other people\'s content.'
+    else:
+        links_analysis = 'Atleast you should be linking to content.'
+
+    # Sentence Length
+    good_sentence_length = []
+    bad_sentence_length = []
+
+    for paragraph in paragraphs:
+        paragraph = paragraph.text
+        sentences = re.split(r'[.!?]+', paragraph)
+
+        for sentence in sentences:
+            if len(sentence) < 20:
+                good_sentence_length.append(sentence)
+            else:
+                bad_sentence_length.append(sentence)
+
+    sentence_length = 'Your content has ' + str(len(sentences)) + " sentences, only " + str(len(good_sentence_length)) + " sentences contains the recommended number of words (less than 20) while " + str(len(bad_sentence_length)) + " sentences contain much words than recommended."
+
+    return render_template('analyse.html', links_analysis=links_analysis, sentence_length=sentence_length, headline_score=headline_score, check_image=check_image, number_in_headline=number_in_headline, heading_focus_keyword=heading_focus_keyword, heading_analysis=heading_analysis, keyword_in_first_paragraph=keyword_in_first_paragraph, paragraph_analysis=paragraph_analysis, uncommon_word_analysis=uncommon_word_analysis, common_word_analysis=common_word_analysis, power_word_analysis=power_word_analysis, keyword_in_headline=keyword_in_headline, word_length_for_headline=word_length_for_headline, reading_ease_score=reading_ease_score, grade_level_score=grade_level_score, content_word_length=content_word_length)
     
 if __name__ == "__main__":
     app.run(debug=True)
